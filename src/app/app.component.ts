810c27c98039bb8ed902641f1f9207378d2b6dec
Marco Lopez <mamorales@todoensubastas.com.mx>
2018-10-12 10:00:34 -0500

    contador-decoradoresinput-output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Store, Action } from '@ngrx/store';
+import { IncrementarAction, DecrementarAction } from './contador/contador.actions';
+
+interface AppState { 
+  contador:  number
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  contador:  number;
+
+  constructor(private store: Store<AppState>)  { 
+    // para obtener datos en forma de objeto... 
+
+    // this.store.subscribe(state =>{ 
+    //   console.log(state);
+    //   this.contador = state.contador;
+    // })
+
+    ///// para obtener los datos no como objetos si no de manera directa el valor de la propiedad
+    this.store.select('contador').subscribe(
+        contador => { 
+          this.contador =  contador;
+          console.log('Valor directo', contador);
+        }
+    )
+
+  }
   title = 'app';
+
+  incrementar() { 
+  // console.log('aqui') (1)
+  // this.contador ++;
+    
+    // const accion: Action = { (2)
+    //   type: 'INCREMENTAR'
+    // };
+
+    const accion = new IncrementarAction();
+    this.store.dispatch(accion);
+  
+  }
+
+  decrementar() { 
+  
+    // console.log('aqui') (1)
+    // this.contador --;
+
+    // const accion: Action = { (2)
+    //   type: 'DECREMENTAR'
+    // }
+    const accion = new DecrementarAction();
+    this.store.dispatch(accion);
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+/// ngrx
+import { StoreModule } from '@ngrx/store';
+import { contadorReducer } from './contador/contador.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+
 import { AppComponent } from './app.component';
+import { HijoComponent } from './contador/hijo/hijo.component';
+import { NietoComponent } from './contador/nieto/nieto.component';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HijoComponent,
+    NietoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ contador: contadorReducer }),
+    StoreDevtoolsModule.instrument({ 
+      maxAge: 25, /// el numerode estado o accionoes que mantenga 25 accciones
+      logOnly: environment.production ///  es una bandea que impidira al usuario que no pueda hacer modificaciones 
+    }),  
   ],
   providers: [],
   bootstrap: [AppComponent]
